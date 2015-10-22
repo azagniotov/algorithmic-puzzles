1eeaab7d6cdf2740da411086fa9f1301c9ceba69
Alexander Zagniotov <azagniotov@gmail.com>
2015-10-22 15:09:44 -0700

    Rotate array

---
+++ rotate-array/MatrixFlipper.java
@@ -0,0 +1,78 @@
+
+public class MatrixFlipper {
+
+
+   public static void main(final String[] args) {
+      final Matrix matrix = new Matrix(6, 6);
+      matrix.dump();
+      //matrix.rotateCounterClockWise();
+      matrix.rotateClockWise();
+      matrix.dump();
+   }
+
+   private static class Matrix {
+
+      private final int columns;
+      private final int rows;
+      private final int[][] matrix;
+
+      Matrix(final int columns, final int rows) {
+         this.columns = columns;
+         this.rows = rows;
+         matrix = new int[this.rows][this.columns];
+         init();
+      }
+
+      void rotateCounterClockWise() {
+         int cache;
+
+         for (int row = 0; row < this.rows / 2; row++) {
+            for (int col = 0; col < Math.ceil(((double) this.columns) / 2.0); col++) {
+               cache = matrix[row][col];
+
+               matrix[row][col] = matrix[col][this.rows - 1 - row];
+               matrix[col][this.rows - 1 - row] = matrix[this.rows - 1 - row][this.columns - 1 - col];
+               matrix[this.rows - 1 - row][this.columns - 1 - col] = matrix[this.columns - 1 - col][row];
+               matrix[this.columns - 1 - col][row] = cache;
+
+               System.out.println("Cached: " + cache);
+            }
+         }
+      }
+
+      void rotateClockWise() {
+         int cache;
+         for (int row = 0; row < this.rows / 2; row++) {
+            for (int col = 0; col < Math.ceil(((double) this.columns) / 2.0); col++) {
+               cache = matrix[row][col];
+
+               matrix[row][col] = matrix[this.columns - 1 - col][row];
+               matrix[this.columns - 1 - col][row] = matrix[this.rows - 1 - row][this.columns - 1 - col];
+               matrix[this.rows - 1 - row][this.columns - 1 - col] = matrix[col][this.rows - 1 - row];
+               matrix[col][this.rows - 1 - row] = cache;
+
+               System.out.println("Cached: " + cache);
+            }
+         }
+      }
+
+      void dump() {
+         for (int row = 0; row < this.rows; row++) {
+            for (int col = 0; col < this.columns; col++) {
+               System.out.print("__" + matrix[row][col] + "__\t");
+            }
+            System.out.println();
+         }
+         System.out.println();
+      }
+
+      private void init() {
+         int counter = 1;
+         for (int row = 0; row < this.rows; row++) {
+            for (int col = 0; col < this.columns; col++) {
+               matrix[row][col] = counter++;
+            }
+         }
+      }
+   }
+}
---
+++ rotate-array/README.md
@@ -0,0 +1,18 @@
+Given an array 6x6:
+
+|    |    |    |    |    |    |
+|----|----|----|----|----|----|
+| __1__  | __2__  | __3__  | 4   | 5   | 6  |
+| __7__  | __8__  | __9__     | 10  | 11  | 12 |   
+| __13__ | __14__ | __15__    | 16  | 17  | 18 |   
+| 19  | 20  | 21  | 22  | 23  | 24  |   
+| 25  | 26  | 27  | 28  | 29  | 30  |  
+| 31  | 32  | 33  | 34  | 35  | 36  |
+
+The nested loop iterates over an `array.length / 2` in `y/rows` and `x/columns` directions. 
+In others words, a `quarter` of the above 2D array (see the numbers in bold):
+
+The values cached in the constant space in each iteration in the following order:
+1 -> 2 -> 3 ->
+7 -> 8 -> 9 ->
+13 -> 14 -> 15 
