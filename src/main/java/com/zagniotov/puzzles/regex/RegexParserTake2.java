1f26ae16881f2f2908e9c71d408c6289f825a0d5
Alexander Zagniotov <azagniotov@gmail.com>
2016-04-14 11:47:03 -0700

    Added second take on regex parser

---
+++ src/main/java/com/zagniotov/puzzles/regex/RegexParserTake2.java
@@ -0,0 +1,70 @@
+package com.zagniotov.puzzles.regex;
+
+/**
+ * -    . matches any single character
+ * -    ^ matches the beginning of the input string
+ * -    * matches zero or more occurrences of the previous character
+ */
+class RegexParserTake2 {
+
+    RegexParserTake2() {
+
+    }
+
+    boolean isMatch(final String candidate, final String pattern) {
+
+        if (candidate.isEmpty() && pattern.isEmpty()) {
+            return true;
+        } else if (pattern.isEmpty()) {
+            return false;
+        } else {
+            if (pattern.charAt(0) == '^') {
+                return isMatchHere(candidate, pattern.substring(1));
+            } else {
+                int i = 0;
+                while (i < candidate.length()) {
+                    if (isMatchHere(candidate, pattern)) {
+                        return true;
+                    }
+                    i++;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    boolean isMatchHere(final String candidate, final String pattern) {
+        if (candidate.isEmpty() && pattern.isEmpty()) {
+            return true;
+        } else if (pattern.isEmpty()) {
+            return false;
+        }
+
+        if (pattern.length() == 1 && pattern.charAt(0) == '*') {
+            return true;
+        }
+
+        if (pattern.length() > 1 && pattern.charAt(0) == '*') {
+            return matchStar(candidate, pattern.substring(1));
+        } else if (pattern.length() > 2 && pattern.charAt(1) == '*') {
+            return matchStar(candidate, pattern.substring(2));
+        }
+        if (candidate.length() > 0 && (pattern.charAt(0) == '.' || pattern.charAt(0) == candidate.charAt(0))) {
+            return isMatchHere(candidate.substring(1), pattern.substring(1));
+        }
+        return false;
+    }
+
+    boolean matchStar(final String candidate, final String pattern) {
+
+        int i = 0;
+        while (i < candidate.length() && (candidate.charAt(i) == pattern.charAt(0) || pattern.charAt(0) == '.')) {
+            if (isMatchHere(candidate, pattern)) {
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/com/zagniotov/puzzles/regex/RegexParserTake2Test.java
@@ -0,0 +1,30 @@
+package com.zagniotov.puzzles.regex;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RegexParserTake2Test {
+
+    @Test
+    public void testIsMatch() throws Exception {
+        final RegexParserTake2 regexParserTake2 = new RegexParserTake2();
+
+
+        assertTrue(regexParserTake2.isMatch("abc", "^abc"));
+        assertTrue(regexParserTake2.isMatch("abc", "abc"));
+        assertTrue(regexParserTake2.isMatch("", ""));
+        assertTrue(regexParserTake2.isMatch("abc", "*abc"));
+        assertTrue(regexParserTake2.isMatch("abc", "w*abc"));
+        assertTrue(regexParserTake2.isMatch("abc", "w*.bc"));
+        assertTrue(regexParserTake2.isMatch("abc", "*"));
+        assertTrue(regexParserTake2.isMatch("abc", "abc*"));
+        assertTrue(regexParserTake2.isMatch("a", "a"));
+        assertTrue(regexParserTake2.isMatch("ab", "a*"));
+
+
+        assertFalse(regexParserTake2.isMatch("ab", "*a"));
+        assertFalse(regexParserTake2.isMatch("abc", ""));
+        assertFalse(regexParserTake2.isMatch("", "abc"));
+    }
+}
