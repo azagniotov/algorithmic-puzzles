cdf54fdf8832d9885643067d61a3ede615782747
Alexander Zagniotov <azagniotov@gmail.com>
2016-04-19 16:16:26 -0700

    Added a javadoc comment to EvenOddSortTake1

--- src/main/java/com/zagniotov/puzzles/strings/EvenOddSortTake1.java
+++ src/main/java/com/zagniotov/puzzles/strings/EvenOddSortTake1.java
@@ -8,6 +8,8 @@ package com.zagniotov.puzzles.strings;
  * - The running time is O(n) and the space complexity is O(n) where 'n' is number of characters in the given string
  * - ascii values for numeric characters from 0 to 9, are respectively from 48 to 57, i.e.:
  * a numeric char with an even numeric value has an ascii even value
+ *
+ * @see EvenOddSortTake2#sort(String) for a more clean & consized solution
  */
 class EvenOddSortTake1 {
 
@@ -25,26 +27,26 @@ class EvenOddSortTake1 {
         int rightPointer = chars.length - 1;
 
         while (leftPointer < rightPointer) {
-            int left = (int) chars[leftPointer];
+            char left = chars[leftPointer];
             if (isEven(left)) {
                 ++leftPointer;
             } else {
                 // the 'left' char is odd, lets start looking form the other end
-                int right = (int) chars[rightPointer];
+                char right = chars[rightPointer];
                 if (isEven(right)) {
-                    chars[leftPointer] = (char) right;
-                    chars[rightPointer] = (char) left;
+                    chars[leftPointer] = right;
+                    chars[rightPointer] = left;
                     --rightPointer;
                 } else {
                     // keep iterating from the end if a char is odd & while the right pointer larger than left
                     while (leftPointer < rightPointer && isOdd(right)) {
-                        right = (int) chars[--rightPointer];
+                        right = chars[--rightPointer];
                     }
 
                     // if we broke out because the pointer condition is false, swap only if the the char is even
                     if (isEven(right)) {
-                        chars[leftPointer] = (char) right;
-                        chars[rightPointer] = (char) left;
+                        chars[leftPointer] = right;
+                        chars[rightPointer] = left;
                     }
                 }
             }
