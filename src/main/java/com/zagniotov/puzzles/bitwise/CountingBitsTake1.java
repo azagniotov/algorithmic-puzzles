ca5fc565d93682f4193afa43df40aaf511c7ae8f
Alexander Zagniotov <azagniotov@gmail.com>
2016-04-25 18:36:37 -0700

    Added CountingBitsTake1

---
+++ src/main/java/com/zagniotov/puzzles/bitwise/CountingBitsTake1.java
@@ -0,0 +1,44 @@
+package com.zagniotov.puzzles.bitwise;
+
+/**
+ * Given a non negative integer number num.
+ * <p>
+ * For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation
+ * and return them as an array. Example:
+ * <p>
+ * For num = 5 you should return [0,1,1,2,1,2].
+ * <p>
+ * 00000000  == 0
+ * 00000001  == 1
+ * 00000010  == 2
+ * 00000011  == 3
+ * 00000100  == 4
+ * 00000101  == 5
+ * 00000110  == 6
+ * 00000111  == 7
+ * 00001000  == 8
+ * 00001001  == 9
+ * 00001010  == 10
+ * 00001011  == 11
+ * 00001100  == 12
+ * 00001101  == 13
+ *
+ * This is an example of dynamic programming where whatever was produced earlier is used. Running time is O(n)
+ */
+public class CountingBitsTake1 {
+
+    CountingBitsTake1() {
+
+    }
+
+    int[] countBits(final int num) {
+        final int[] result = new int[num + 1];
+        result[0] = 0;
+        for (int idx = 1; idx <= num; idx++) {
+            int andResult = idx & (idx - 1);
+            result[idx] = result[andResult] + 1;
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/com/zagniotov/puzzles/bitwise/CountingBitsTake1Test.java
@@ -0,0 +1,25 @@
+package com.zagniotov.puzzles.bitwise;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class CountingBitsTake1Test {
+
+    @Test
+    public void testCountBits() throws Exception {
+        final CountingBitsTake1 countingBitsTake1 = new CountingBitsTake1();
+        int[] result = countingBitsTake1.countBits(5);
+
+        assertEquals(Arrays.toString(new int[]{0, 1, 1, 2, 1, 2}), Arrays.toString(result));
+
+        result = countingBitsTake1.countBits(7);
+        System.out.println(result[7 & 6]);
+        System.out.println(result[7 & 6] + 1);
+
+        assertEquals(Arrays.toString(new int[]{0, 1, 1, 2, 1, 2, 2, 3}), Arrays.toString(result));
+    }
+}
