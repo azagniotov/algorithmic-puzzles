3c49880de016152e84b172768d873193db0aed75
Alexander Zagniotov <azagniotov@gmail.com>
2016-04-27 13:07:31 -0700

    comments

--- src/main/java/com/zagniotov/puzzles/bitwise/CountingOneBitsTake1.java
+++ src/main/java/com/zagniotov/puzzles/bitwise/CountingOneBitsTake1.java
@@ -1,30 +1,5 @@
 package com.zagniotov.puzzles.bitwise;
 
-/**
- * Given a non negative integer number num.
- * <p>
- * For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation
- * and return them as an array. Example:
- * <p>
- * For num = 5 you should return [0,1,1,2,1,2].
- * <p>
- * 00000000  == 0
- * 00000001  == 1
- * 00000010  == 2
- * 00000011  == 3
- * 00000100  == 4
- * 00000101  == 5
- * 00000110  == 6
- * 00000111  == 7
- * 00001000  == 8
- * 00001001  == 9
- * 00001010  == 10
- * 00001011  == 11
- * 00001100  == 12
- * 00001101  == 13
- * <p>
- * This is an example of dynamic programming where whatever was produced earlier is used. Running time is O(n)
- */
 public class CountingOneBitsTake1 {
 
     CountingOneBitsTake1() {
@@ -84,6 +59,31 @@ public class CountingOneBitsTake1 {
         return counter;
     }
 
+    /**
+     * Given a non negative integer number num.
+     * <p>
+     * For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation
+     * and return them as an array. Example:
+     * <p>
+     * For num = 5 you should return [0,1,1,2,1,2].
+     * <p>
+     * 00000000  == 0
+     * 00000001  == 1
+     * 00000010  == 2
+     * 00000011  == 3
+     * 00000100  == 4
+     * 00000101  == 5
+     * 00000110  == 6
+     * 00000111  == 7
+     * 00001000  == 8
+     * 00001001  == 9
+     * 00001010  == 10
+     * 00001011  == 11
+     * 00001100  == 12
+     * 00001101  == 13
+     * <p>
+     * This is an example of dynamic programming where whatever was produced earlier is used. Running time is O(n)
+     */
     int[] countOneBits4(final int num) {
         final int[] result = new int[num + 1];
         result[0] = 0;
