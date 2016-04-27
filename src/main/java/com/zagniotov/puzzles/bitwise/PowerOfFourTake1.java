b974782699a7c74f1086b58ad8120b5b068d5a38
Alexander Zagniotov <azagniotov@gmail.com>
2016-04-27 12:49:15 -0700

    Added PowerOfFourTake1

--- src/main/java/com/zagniotov/puzzles/bitwise/PowerOfFourTake1.java
+++ src/main/java/com/zagniotov/puzzles/bitwise/PowerOfFourTake1.java
@@ -11,8 +11,21 @@ public class PowerOfFourTake1 {
      * Power of 2 numbers: 1, 2, 4,  8,  16
      * Power of 4 numbers: 1, 4, 16, 64, 256
      */
-    boolean isPowerOfFour(int number) {
+    boolean isPowerOfFour1(int number) {
+        // the first two conditions are for power of 2
         // the operator hierarchy places '-' above '&'
         return (number & number - 1) == 0 && number > 0 && (number - 1) % 3 == 0;
+    }
+
+    /**
+     * Power of 2 numbers: 1, 2, 4,  8,  16
+     * Power of 4 numbers: 1, 4, 16, 64, 256
+     */
+    boolean isPowerOfFour2(int number) {
+        // the first two conditions are for power of 2
+        // the operator hierarchy places '-' above '&'
+        // binary representation of 5 is 0101. So 0x55555555 has 16 ones, 16 zeros and the ones,
+        // zeros take alternate positions: 01010101010101010101010101010101
+        return (number & number - 1) == 0 && number > 0 && (number & 0x55555555) != 0;
     }
 }
--- src/test/java/com/zagniotov/puzzles/bitwise/PowerOfFourTake1Test.java
+++ src/test/java/com/zagniotov/puzzles/bitwise/PowerOfFourTake1Test.java
@@ -9,13 +9,24 @@ public class PowerOfFourTake1Test {
     PowerOfFourTake1 powerOfFourTake1 = new PowerOfFourTake1();
 
     @Test
-    public void testIsPowerOfFour() throws Exception {
-        assertTrue(powerOfFourTake1.isPowerOfFour(1));
-        assertTrue(powerOfFourTake1.isPowerOfFour(4));
-        assertTrue(powerOfFourTake1.isPowerOfFour(16));
-        assertTrue(powerOfFourTake1.isPowerOfFour(64));
-        assertTrue(powerOfFourTake1.isPowerOfFour(256));
+    public void testIsPowerOfFour1() throws Exception {
+        assertTrue(powerOfFourTake1.isPowerOfFour1(1));
+        assertTrue(powerOfFourTake1.isPowerOfFour1(4));
+        assertTrue(powerOfFourTake1.isPowerOfFour1(16));
+        assertTrue(powerOfFourTake1.isPowerOfFour1(64));
+        assertTrue(powerOfFourTake1.isPowerOfFour1(256));
 
-        assertFalse(powerOfFourTake1.isPowerOfFour(512));
+        assertFalse(powerOfFourTake1.isPowerOfFour1(512));
+    }
+
+    @Test
+    public void testIsPowerOfFour2() throws Exception {
+        assertTrue(powerOfFourTake1.isPowerOfFour2(1));
+        assertTrue(powerOfFourTake1.isPowerOfFour2(4));
+        assertTrue(powerOfFourTake1.isPowerOfFour2(16));
+        assertTrue(powerOfFourTake1.isPowerOfFour2(64));
+        assertTrue(powerOfFourTake1.isPowerOfFour2(256));
+
+        assertFalse(powerOfFourTake1.isPowerOfFour2(512));
     }
 }
