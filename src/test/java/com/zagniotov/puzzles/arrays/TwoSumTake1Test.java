11ca1a1f853313766c2d23adb3bc536d501d89c8
Alexander Zagniotov <azagniotov@gmail.com>
2016-04-26 21:51:51 -0700

    Added TwoSumTake1 & alculateFactorialTake1

---
+++ src/test/java/com/zagniotov/puzzles/arrays/TwoSumTake1Test.java
@@ -0,0 +1,22 @@
+package com.zagniotov.puzzles.arrays;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class TwoSumTake1Test {
+
+    @Test
+    public void testTwoSum() throws Exception {
+        TwoSumTake1 twoSumTake1 = new TwoSumTake1();
+
+        int[] indices = twoSumTake1.twoSum(new int[]{2, 7, 11, 15}, 9);
+        assertEquals(Arrays.toString(new int[]{0, 1}), Arrays.toString(indices));
+
+        indices = twoSumTake1.twoSum(new int[]{-3, 4, 3, 90}, 0);
+        assertEquals(Arrays.toString(new int[]{0, 2}), Arrays.toString(indices));
+    }
+}
---
+++ src/test/java/com/zagniotov/puzzles/math/CalculateFactorialTake1Test.java
@@ -0,0 +1,20 @@
+package com.zagniotov.puzzles.math;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class CalculateFactorialTake1Test {
+
+    @Test
+    public void testFactorial() throws Exception {
+        CalculateFactorialTake1 calculateFactorialTake1 = new CalculateFactorialTake1();
+
+        int result = calculateFactorialTake1.factorial(3);
+        assertEquals(6, result);
+
+        result = calculateFactorialTake1.factorial(4);
+        assertEquals(24, result);
+    }
+}
