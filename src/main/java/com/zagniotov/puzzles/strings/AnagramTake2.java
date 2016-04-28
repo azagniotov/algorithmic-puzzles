1c135823c6ca2b103b38627fefdfa4a6b5cb4ce5
Alexander Zagniotov <azagniotov@gmail.com>
2016-04-27 19:56:22 -0700

    Added AnagramTake2

--- src/main/java/com/zagniotov/puzzles/strings/AnagramTake2.java
+++ src/main/java/com/zagniotov/puzzles/strings/AnagramTake2.java
@@ -1,42 +1,33 @@
 package com.zagniotov.puzzles.strings;
 
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 class AnagramTake2 {
 
     AnagramTake2() {
 
     }
 
-    // Assumes that strings will contain ASCII characters [0-9A-Z]
+    // Assumes that strings will contain only the same cased ASCII characters
     boolean isAnagram(final String first, final String second) {
         if (first.length() != second.length()) {
             return false;
         }
 
-        final char[] firstChars = first.toUpperCase().toCharArray();
-        final int[] firstCharsCounter = new int[91]; //Enough space to cover numeric values of ASCII characters [0-9A-Z]
+        final char[] firstChars = first.toCharArray();
+        final char[] secondChars = second.toCharArray();
+        final int[] mapping = new int[127]; //Enough space to cover numeric values of ASCII characters [0-9a-zA-Z]
+        int counter = 0;
 
-        for (char ch : firstChars) {
-            int count = firstCharsCounter[ch] != 0 ? firstCharsCounter[ch] : 0;
-            firstCharsCounter[ch] = count + 1;
-        }
+        for (int idx = 0; idx < firstChars.length; idx++) {
 
-        final char[] secondChars = second.toUpperCase().toCharArray();
-        final int[] secondCharsCounter = new int[91]; //Enough space to cover numeric values of ASCII characters [0-9A-Z]
-
-        for (char ch : secondChars) {
-            if (firstCharsCounter[ch] == 0) {
-                return false;
-            } else {
-                int count = secondCharsCounter[ch] != 0 ? secondCharsCounter[ch] : 0;
-                secondCharsCounter[ch] = count + 1;
+            if (++mapping[firstChars[idx]] == 1) {
+                counter++;
+            }
+            if (--mapping[secondChars[idx]] == 0) {
+                counter--;
             }
         }
 
-        return Arrays.equals(firstCharsCounter, secondCharsCounter);
+        return counter == 0;
     }
 }
--- src/test/java/com/zagniotov/puzzles/strings/AnagramTake2Test.java
+++ src/test/java/com/zagniotov/puzzles/strings/AnagramTake2Test.java
@@ -13,15 +13,12 @@ public class AnagramTake2Test {
 
         final AnagramTake2 anagramTake2 = new AnagramTake2();
 
-        assertTrue(anagramTake2.isAnagram("MotherInLaw", "HitlerWoman"));
-        assertTrue(anagramTake2.isAnagram("keEp", "peeK"));
-        assertTrue(anagramTake2.isAnagram("SiLeNtCAT", "LisTenAcT"));
-        assertTrue(anagramTake2.isAnagram("DebitCard", "BadCredit"));
-        assertTrue(anagramTake2.isAnagram("SchoolMASTER", "TheClassROOM"));
-        assertTrue(anagramTake2.isAnagram("DORMITORY", "DirtyRoom"));
+        assertTrue(anagramTake2.isAnagram("motherinlaw", "hitlerwoman"));
+        assertTrue(anagramTake2.isAnagram("debitcard", "badcredit"));
+        assertTrue(anagramTake2.isAnagram("DORMITORY", "DIRTYROOM"));
         assertTrue(anagramTake2.isAnagram("ASTRONOMERS", "NOMORESTARS"));
 
-        assertFalse(anagramTake2.isAnagram("Toss", "Shot"));
+        assertFalse(anagramTake2.isAnagram("toss", "snot"));
         assertFalse(anagramTake2.isAnagram("joy", "enjoy"));
         assertFalse(anagramTake2.isAnagram("joy", "enjoy"));
         assertFalse(anagramTake2.isAnagram("aaaaa", "baaaa"));
